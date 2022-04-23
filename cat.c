fe9c1fa5d77e33d2bc9b5024466c38486358b20f
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-04-23 19:01:42 +0200

    Pipe | simulation added

--- cat.c
+++ cat.c
@@ -276,7 +276,7 @@ int main(int argc, char* argv[]){
 
 // when sending arrays through a pipe, we should also send the array size through the pipe
 
-
+/*
 // sending a string through a pipe
 
 int main(int argc, char* argv[]){
@@ -325,12 +325,48 @@ int main(int argc, char* argv[]){
 
   return 0;
 }
+*/
 
 
 //Simulating the pipe "|" operator
-/*
+
 int main(int argc, char* argv[]){
+  int fd[2];
+  if(pipe(fd) == -1){
+    return 1;
+  }
+
+  int pid1 = fork();
+  if(pid1<0){
+    return 2;
+  }
+  if(pid1 == 0){
+    // child process 1 (ping)
+    //reroute output
+    dup2(fd[1], STDOUT_FILENO); //replaces STDOUT with the writing end of the pipe
+    close(fd[0]);
+    close(fd[1]);
+    execlp("ping", "ping", "-c", "5", "google.com", NULL);
+    // no need to return anything here or add if(pid>0) because the child process got replaced by ping. It won't go further
+  }
+
+  int pid2 = fork();
+  if(pid2<0){
+    return 3;
+  }
+  if(pid2==0){
+    //Child process 2 (grep)
+    dup2(fd[0], STDIN_FILENO); //replaces STDIN with the reading end of the pipe
+    close(fd[0]);
+    close(fd[1]);
+    execlp("grep", "grep", "rtt", NULL);
+  }
+
+  close(fd[0]);
+  close(fd[1]);
+
+  waitpid(pid1, NULL, 0);
+  waitpid(pid2, NULL, 0);
 
   return 0;
 }
-*/

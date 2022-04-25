e735d49587112b00dd2deed0409fae30b3668e5f
DzenisKajevic <dzenis.kajevic@stu.ibu.edu.ba>
2022-04-25 11:35:14 +0200

    Working with multiple pipes

--- cat.c
+++ cat.c
@@ -327,7 +327,7 @@ int main(int argc, char* argv[]){
 }
 */
 
-
+/*
 //Simulating the pipe "|" operator
 
 int main(int argc, char* argv[]){
@@ -367,6 +367,94 @@ int main(int argc, char* argv[]){
 
   waitpid(pid1, NULL, 0);
   waitpid(pid2, NULL, 0);
+
+  return 0;
+}*/
+
+//working with multiple pipes
+
+int main(int argc, char* argv[]){
+  int fd[3][2];
+  // fd[0][0] child 1 reads parent-child1 pipe
+  // fd[0][1] parent writes to parent-child1 pipe
+  // fd[1][0] child 2 reads child 1-2 pipe
+  // fd[1][1] child 1 writes child 1-2 pipe
+  // fd[2][0] parent reads child2-parent pipe
+  // fd[2][1] child2 writes child2-parent pipe
+  int i;
+  for(i = 0; i < 3; i++){
+    if(pipe(fd[i])<0){ //it's recommended to have separate error codes for each failed pipe, and not like this, but it doesn't matter atm
+      return 1;
+    }
+  }
+
+  int pid1 = fork();
+  if(pid1 < 0) { //should close the pipes before returning, but too long
+    return 2;
+  }
+  if (pid1 == 0){
+    //Child process 1
+    close(fd[0][1]);
+    close(fd[1][0]);
+    close(fd[2][0]);
+    close(fd[2][1]);
+    int x;
+    if(read(fd[0][0], &x, sizeof(x))<0){
+      return 3;
+    }
+    x+=5;
+    if(write(fd[1][1], &x, sizeof(x))<0){
+      return 4;
+    }
+    close(fd[0][0]);
+    close(fd[1][1]);
+    return 0;
+  }
+
+  int pid2 = fork();
+  if(pid2<0){
+    return 5;
+  }
+  if(pid2 == 0){
+    //Child process 2
+    close(fd[0][0]);
+    close(fd[0][1]);
+    close(fd[1][1]);
+    close(fd[2][0]);
+    int x;
+    if(read(fd[1][0], &x, sizeof(x))<0){
+      return 6;
+    }
+    x+=5;
+    if(write(fd[2][1], &x, sizeof(x))<0){
+      return 7;
+    }
+    close(fd[1][0]);
+    close(fd[2][1]);
+    return 0;
+  }
+
+  //parent process
+  close(fd[0][0]);
+  close(fd[1][0]);
+  close(fd[1][1]);
+  close(fd[2][1]);
+  int x = 0;
+  printf("Input number: ");
+  scanf("%d", &x);
+  if(write(fd[0][1], &x, sizeof(x))<0){
+    return 8;
+  }
+  if(read(fd[2][0], &x, sizeof(x))<0){
+    return 9;
+  }
+  printf("Result is %d\n", x);
+  close(fd[0][1]);
+  close(fd[2][0]);
+
+  waitpid(pid1, NULL, 0);
+  waitpid(pid2, NULL, 0);
+
 
   return 0;
 }
